6554d30e8ff54f53233571b73bb4e06d10fb9f9b
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2025-12-22 23:04:46 +0800

    new: Entity class

---
+++ IAmStillAlive/Player.py
@@ -0,0 +1,25 @@
+import pygame
+from Entity import Entity
+
+
+class Player(Entity):
+    def __init__(self, screen):
+        super().__init__()
+        self.screen = screen
+        self.image = pygame.image.load("resources/player.png").convert_alpha()
+        self.up = False
+        self.down = False
+        self.left = False
+        self.right = False
+        self.speed = 2
+
+    def update(self, delta_time):
+        if self.up:
+            self.y -= (60 * self.speed) * delta_time
+        if self.down:
+            self.y += (60 * self.speed) * delta_time
+        if self.left:
+            self.x -= (60 * self.speed) * delta_time
+        if self.right:
+            self.x += (60 * self.speed) * delta_time
+        self.screen.blit(self.image, (self.x, self.y))
--- IAmStillAlive/main.py
+++ IAmStillAlive/main.py
@@ -1,53 +1,40 @@
 import asyncio
 import pygame
+from Player import Player
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
-player = pygame.image.load("player.png").convert_alpha()
 clock = pygame.time.Clock()
 
 
 async def main():
     delta_time = 0.1
-    x = 0
-    y = 0
-    up = False
-    down = False
-    left = False
-    right = False
+    player = Player(screen)
     running = True
     while running:
         screen.fill((255, 255, 255))
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w:
-                    up = True
+                    player.up = True
                 if event.key == pygame.K_a:
-                    left = True
+                    player.left = True
                 if event.key == pygame.K_s:
-                    down = True
+                    player.down = True
                 if event.key == pygame.K_d:
-                    right = True
+                    player.right = True
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_w:
-                    up = False
+                    player.up = False
                 if event.key == pygame.K_a:
-                    left = False
+                    player.left = False
                 if event.key == pygame.K_s:
-                    down = False
+                    player.down = False
                 if event.key == pygame.K_d:
-                    right = False
+                    player.right = False
             if event.type == pygame.QUIT:
                 running = False
-        if up:
-            y -= 60 * delta_time
-        if down:
-            y += 60 * delta_time
-        if left:
-            x -= 60 * delta_time
-        if right:
-            x += 60 * delta_time
-        screen.blit(player, (x, y))
+        player.update(delta_time)
         pygame.display.flip()
         delta_time = clock.tick(60) / 1000
         delta_time = max(0.001, min(0.1, delta_time))
