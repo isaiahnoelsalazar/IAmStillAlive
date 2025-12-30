020e5c6ce11befcd6f7db627bdbc1e9ab2edfa79
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2025-12-30 17:24:33 +0800

    update: change Scenario2 data

--- IAmStillAlive/scenarios/Scenario2.py
+++ IAmStillAlive/scenarios/Scenario2.py
@@ -8,5 +8,9 @@ class Scenario2(Scenario):
         super().__init__(camera)
         self.map_manager.load(1)
         player = Player(self.camera)
+        player.set_spawn(
+            x = 144,
+            y = 144
+        )
         self.entity_list.append(player)
         self.camera.set_target(player)
